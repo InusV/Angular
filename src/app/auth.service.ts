a2a29a64cf4a119ab56e8f7ad4849c5935c392df
InusV <inus.vermeulen2917@gmail.com>
2020-11-19 10:36:31 +0200

    Login changes

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getUserdetails() {
     //Post these details to API server return user info if correct
+
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,8 +12,11 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
-  loginUser(event) {
+  loginUser(event:any) {
     event.preventDefault()
-    console.log(event)
+    const target = event.target
+    const username = target.querySelector('#username').value
+    const password = target.querySelector('#password').value
+    console.log(username, password)
   }
 }
